15a10282930eef10ac1871c4a7437cf47fd83a74
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-11-17 21:06:16 +0100

    add details to Main

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+export function Main() {
+  return (
+    <>
+      <form className="form">
+        <label>Motto</label>
+        <input />
+        <label>Notes</label>
+        <textarea />
+        <button>create</button>
+      </form>
+      <section className="entries"></section>
+    </>
+  );
+}
