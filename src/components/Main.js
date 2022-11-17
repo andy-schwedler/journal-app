357d63667560089e938fddf90115b482b1da9c48
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-11-17 21:56:48 +0100

    add style to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,18 @@
 export function Main() {
   return (
     <>
-      <form className="form">
-        <label>Motto</label>
-        <input />
-        <label>Notes</label>
-        <textarea />
-        <button>create</button>
-      </form>
+      <section className="section">
+        <fieldset>
+          <legend>New Entry</legend>
+          <form className="form--">
+            <label htmlFor="Motto">Motto</label>
+            <input name="Motto" />
+            <label htmlFor="Notes">Notes</label>
+            <textarea name="Notes" />
+            <button>create</button>
+          </form>
+        </fieldset>
+      </section>
       <section className="entries"></section>
     </>
   );
